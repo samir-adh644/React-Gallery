709a04eeb73a3ce090e130e048759cc942d9a96e
Samir Adhikari <081bct071@ioepc.edu.np>
2025-12-18 20:56:15 +0545

    Making interface and calling api

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import {useState} from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [userData,setUserData]=([]);
+
+
+  const getData = async ()=>{
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
+    setUserData(response.data)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="main bg-black text-white h-screen">
+
+      <button 
+      onClick={getData}
+      className="bg-green-600 px-5 py-2 rounded text-white m-4 active:scale-95 ">
+        Get Data
+      </button>
+
+    </div>
+     
     </>
   )
 }
